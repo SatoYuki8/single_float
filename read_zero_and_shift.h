6dcf38ad9af1a5e6108b5ae7ff6ac72c4c404be8
SatoYuki8 <sato.yuki@arc.cs.okayama-u.ac.jp>
2017-08-12 22:27:15 +0900

    change half prec to single prec

--- read_zero_and_shift.h
+++ read_zero_and_shift.h
@@ -1,8 +1,8 @@
 declare read_zero_and_shift{
-      input frac<13>;
-      input exp<5>;
-      output shifted_frac<13>;
-      output sub_exp<5>;
+      input frac<26>;
+      input exp<8>;
+      output shifted_frac<26>;
+      output sub_exp<8>;
       instrin do;
       
       instr_arg do(frac, exp);
